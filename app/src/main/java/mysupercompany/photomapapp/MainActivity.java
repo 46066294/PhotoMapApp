8ecc52eb1eee36d6b3aaa32ddb6d37b00beb10e3
46066294 <46066294p@iespoblenou.org>
2017-03-02 03:38:56 +0100

    fragment incoming

--- app/src/main/java/mysupercompany/photomapapp/MainActivity.java
+++ app/src/main/java/mysupercompany/photomapapp/MainActivity.java
@@ -1,15 +1,18 @@
 package mysupercompany.photomapapp;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    CustomViewPager viewPager;
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +21,11 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        viewPager = (CustomViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
     @Override
@@ -43,10 +43,24 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.action_nota) {
+            Intent i = new Intent(getApplicationContext(), NoteActivity.class);
+            startActivity(i);
+        } else if (id == R.id.action_foto) {
+            Intent i = new Intent(getApplicationContext(), PhotoActivity.class);
+            startActivity(i);
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void setupViewPager(CustomViewPager upViewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new NoteFragmentAdapter(), "Notas");
+        adapter.addFragment(new MapFragment(), "Mapa");
+        adapter.addFragment(new PhotoFragmentAdapter(), "Fotos");
+
+        upViewPager.setAdapter(adapter);
+        upViewPager.setPagingEnabled(false);
     }
 }
