8a2b2eb664838efd684ed697f72e8682ab6b54c1
46066294 <46066294p@iespoblenou.org>
2017-03-02 03:41:37 +0100

    pojos ready

---
+++ app/src/main/java/mysupercompany/photomapapp/Note.java
@@ -0,0 +1,46 @@
+package mysupercompany.photomapapp;
+
+/**
+ * Created by Mat on 01/03/2017.
+ */
+
+public class Note {
+    String title;
+    String message;
+    Double lat;
+    Double lon;
+
+    public Note(){}
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    public Double getLon() {
+        return lon;
+    }
+
+    public void setLon(Double lon) {
+        this.lon = lon;
+    }
+}
---
+++ app/src/main/java/mysupercompany/photomapapp/Photo.java
@@ -0,0 +1,47 @@
+package mysupercompany.photomapapp;
+
+/**
+ * Created by Mat on 01/03/2017.
+ */
+
+public class Photo {
+
+    Double lat;
+    Double lon;
+    String path;
+    String name;
+
+    public Photo() {}
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    public Double getLon() {
+        return lon;
+    }
+
+    public void setLon(Double lon) {
+        this.lon = lon;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
